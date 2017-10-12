155b3865663baa239e7bcfd69a6907f43c0b5590
razumforce <razum.force@gmail.com>
2017-10-12 19:00:59 +0000

    lesson5 plus add dependent: :destroy to post model in relation to commentaries

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 	belongs_to :user
-	has_many :commentaries
+	has_many :commentaries, dependent: :destroy
 
   validates :title, presence: true
   validates :title, length: {minimum: 5}
