26ceac4d08f5e6d7037fd1049dbbd48f58cf2c99
razumforce <razum.force@gmail.com>
2017-10-14 22:20:20 +0000

    add basic devise functionality as per lesson6 materials

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010221207) do
+ActiveRecord::Schema.define(version: 20171014211334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,18 @@ ActiveRecord::Schema.define(version: 20171010221207) do
     t.boolean "creator", default: false, null: false
     t.boolean "active", default: true, null: false
     t.string "test"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "votes", force: :cascade do |t|
