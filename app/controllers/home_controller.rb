189822bed7a89f7ce3ce449a24fecf9a04dc6a4e
razumforce <razum.force@gmail.com>
2017-10-16 20:18:28 +0000

    login and if needed - new user creation - with FB - working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def show
   	session[:times_her] = 0
-  	session[:a] = 5
+  	# session[:a] = 5
   	session[:times_visited] = if session[:times_visited].nil?
   			1
   		else
